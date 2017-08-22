f3da1524acbae54663c7396507cdcb84353c2691
underc8der <davidn.g0015@gmail.com>
2017-08-22 16:38:19 -0500

    appliying rest controller and mapping message

--- spring-microservice-oatuh-server/src/main/java/com/oreilly/cloud/SpringMicroserviceOatuhServerApplication.java
+++ spring-microservice-oatuh-server/src/main/java/com/oreilly/cloud/SpringMicroserviceOatuhServerApplication.java
@@ -3,10 +3,20 @@ package com.oreilly.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableAuthorizationServer
+@EnableResourceServer
+@RestController
 public class SpringMicroserviceOatuhServerApplication {
+	
+	@RequestMapping("/resources/endpoint")
+	public String foo() {
+		return "This message is protected by the resource server.";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMicroserviceOatuhServerApplication.class, args);
