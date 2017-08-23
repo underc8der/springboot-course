9dc57006471ba062b1187d805670a312506fa6a1
underc8der <davidn.g0015@gmail.com>
2017-08-23 10:33:22 -0500

    adding demo configuration and configuration scanning from spring

---
+++ spring-microservice/src/main/java/com/oreally/cloud/DemoConfiguration.java
@@ -0,0 +1,15 @@
+package com.oreally.cloud;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DemoConfiguration {
+	@Bean
+	public List<String> cats() {
+		return Arrays.asList("Bengal", "Lion");
+	}
+}
--- spring-microservice/src/main/java/com/oreally/cloud/SpringMicroserviceApplication.java
+++ spring-microservice/src/main/java/com/oreally/cloud/SpringMicroserviceApplication.java
@@ -1,7 +1,13 @@
 package com.oreally.cloud;
 
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SpringMicroserviceApplication {
 
+	@Autowired
+	@Resource(name="cats")
+	public List<String> list;
+	
 	@RequestMapping("/")
 	public String greetings() {
-		return "Hello World!";
+		return String.join(", ", list);
 	}
 	
 	public static void main(String[] args) {
